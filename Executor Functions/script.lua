bee8dc00b18384a944d85a6088cd42acc14b3d6a
FurryBoy <bogdanfurry2000@gmail.com>
2024-07-22 12:59:33 +0300

    _

--- Executor Functions/script.lua
+++ Executor Functions/script.lua
@@ -1,5 +1,6 @@
+execName=identityexecutor()
 count=0
 array={}
 for i,_ in pairs(getgenv())do count+=1 array[count]=i..'\n'end
 out=table.concat(array)
-writefile(identityexecutor()..'.txt',out)
+writefile(execName..'.txt','-- '..execName..' executor functions\n\n'..out)
